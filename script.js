4450c8fc117e120ed55b359db817cba343658fe4
Jared Edwards <jredwards519@gmail.com>
2017-01-30 14:17:12 -0500

    Initial Commit linking files

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+
+
+});//end document ready
+
+
+
+/*
+Pseudocode
+Create an input field that will allow user to input a word
+create a variable that will take in the value of the word that was input by user
+    -- MAKE SURE THAT ENTER ALSO WORKS AS A KEY PRESS
+
+when submit button is clicked, hide the submit button and show a new input field that will allow the user guesses
+create a flex box for divs
+create a single line of code that creates a div (with a border and size x X x)
+create a for loop that will generate as many divs in a flexbox as in word.length
+allow user to guess while less than word.length
+
+create display box with remaining guesses
+then worry about hang man picture
+
+
+
+
+*/
